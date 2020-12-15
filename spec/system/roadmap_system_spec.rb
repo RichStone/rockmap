e996073c480b62efeec2101911ec97c68eab5ef0
RichStone <richard.had@hotmail.de>
2020-12-15 18:35:15 +0100

    Fix System test

--- spec/system/roadmap_system_spec.rb
+++ spec/system/roadmap_system_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Roadmap", type: :system do
-  it "enable me to create coins and publish to ebay" do
-    visit "/"
-    expect(page).to have_text('Rockmap')
+  it 'lets a Rockstar create a personal Rockmap' do
+    visit '/'
+    expect(current_path).to eq root_path
+    click_button 'Create Your Rockmap Now!'
 
-    fill_in "The ONE Thing", with: "Building products!"
-    fill_in "Milestone", with: "Rockmap"
-    fill_in "Milestone Date", with: '31.12.2020'
-    fill_in "The ONE Habit", with: "Meditate"
+    expect(current_path).to eq new_roadmap_path
+
+    fill_in 'roadmap[the_one_thing]', with: 'Build things that improve people\'s lives.'
+    fill_in 'roadmap[the_why_of_one_thing]', with: 'Because it rocks!'
+    fill_in 'roadmap[start_date]', with: '31.12.2020'
+    fill_in 'roadmap[the_one_habit]', with: 'Affirm'
+
     click_button 'Go Rock!'
 
-    expect(page).to have_text('Rockmap')
+    expect(page).to have_content 'Rock Time!'
   end
 end
