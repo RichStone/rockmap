b4882c0adb28c19513df20ad9c5d549fa3b88f08
RichStone <richard.had@hotmail.de>
2020-12-26 19:29:01 +0100

    Let user edit their rockmap

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -11,11 +11,29 @@ class RoadmapsController < ApplicationController
 
   def create
     @roadmap = current_user.create_roadmap(roadmap_params)
+
     if @roadmap.save
       redirect_to :action => 'show', success: 'Now you are even more awesome!'
     else
       redirect_to :action => 'new', alert: 'Something went wrong..'
     end
+  end
+end
+
+def edit
+  puts '###', Roadmap.find(params[:id])
+  puts 'UUU', current_user.roadmap
+  @roadmap = Roadmap.find(params[:id])
+  @x = 5
+end
+
+def update
+  @roadmap = current_user.roadmap
+
+  if @roadmap.update_attributes(roadmap_params)
+    redirect_to :action => 'show', success: 'Hopefully you stick to it now! ;)'
+  else
+    redirect_to :action => 'show', alert: 'Something went wrong updating your Rockmap :('
   end
 end
 
