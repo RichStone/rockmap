b5ff29f32fd9f4eaa58a8cfef2a9080fefbaddcf
RichStone <richard.had@hotmail.de>
2020-12-27 19:25:50 +0100

    Let users edit their rockmap

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -18,29 +18,32 @@ class RoadmapsController < ApplicationController
       redirect_to :action => 'new', alert: 'Something went wrong..'
     end
   end
-end
 
-def edit
-  puts '###', Roadmap.find(params[:id])
-  puts 'UUU', current_user.roadmap
-  @roadmap = Roadmap.find(params[:id])
-  @x = 5
-end
-
-def update
-  @roadmap = current_user.roadmap
-
-  if @roadmap.update_attributes(roadmap_params)
-    redirect_to :action => 'show', success: 'Hopefully you stick to it now! ;)'
-  else
-    redirect_to :action => 'show', alert: 'Something went wrong updating your Rockmap :('
+  def edit
+    @roadmap = Roadmap.find(params[:id])
   end
-end
 
-private
+  def update
+    @roadmap = current_user.roadmap
 
-def roadmap_params
-  params.require(:roadmap).permit(
-    :the_one_thing, :the_why_of_one_thing, :the_one_habit, :start_date
-  )
+    if @roadmap.update_attributes(roadmap_params)
+      redirect_to controller: 'roadmaps',
+                  action: 'show',
+                  id: current_user.roadmap.id,
+                  success: 'Hopefully you stick to it now! ;)'
+    else
+      redirect_to controller: 'roadmaps',
+                  action: 'show',
+                  id: current_user.roadmap.id,
+                  alert: 'Something went wrong updating your Rockmap :('
+    end
+  end
+
+  private
+
+  def roadmap_params
+    params.require(:roadmap).permit(
+      :the_one_thing, :the_why_of_one_thing, :the_one_habit, :start_date
+    )
+  end
 end
