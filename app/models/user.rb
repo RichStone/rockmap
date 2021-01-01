a382f10b19cd942ba7a061d76e13beb3a2f7f828
RichStone <richard.had@hotmail.de>
2021-01-01 12:03:25 +0100

    Delete roadmap if user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :roadmap, dependent: delete_all
+  has_one :roadmap, dependent: :destroy
 end
