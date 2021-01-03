818ea6415713d35bbb1c36e8e05d3786d8bf49dd
RichStone <richard.had@hotmail.de>
2021-01-03 21:49:54 +0100

    Send welcome email after user was confirmed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_one :roadmap, dependent: :destroy
+
+  def after_confirmation
+    UserMailer.with(user: self).welcome_email.deliver_later
+  end
 end
