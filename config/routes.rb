6b04e0476ccac814b33d54af66af3f40632c5e3e
RichStone <richard.had@hotmail.de>
2021-01-09 15:45:41 +0100

    Add sidekiq routes and conf

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+  mount Sidekiq::Web => '/sidekiq'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   authenticated :user do
