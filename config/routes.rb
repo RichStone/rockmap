9701c5f59223b00fe32a27ef6304738cd57caaa0
RichStone <richard.had@hotmail.de>
2021-01-16 15:51:11 +0100

    Fix sign out not working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   require 'sidekiq/cron/web'
   mount Sidekiq::Web => '/sidekiq'
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   authenticated :user do
     root :to => 'roadmaps#show', :as => "authenticated_root"
