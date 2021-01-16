8c32e7a76a82638c5747ef07fad8f9aadf560acd
RichStone <richard.had@hotmail.de>
2021-01-16 15:54:06 +0100

    Fix specs

--- spec/requests/roadmap_request_spec.rb
+++ spec/requests/roadmap_request_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe "Roadmaps", type: :request do
+RSpec.describe 'Roadmaps', type: :request do
+  context 'with registered & confirmed user but without a Rockmap' do
+    it 'redirects user on sign in and signs out' do
+      user = create(:confirmed_blank_user )
+      sign_in user
+      get root_path
+      expect(response.status).to be 302
 
+      sign_out user
+      get root_path
+      expect(response.status).to be 200
+    end
+  end
+
+  describe 'GET /' do
+    context 'when user is logged in' do
+
+      before do
+        @user = user_with_roadmap
+        @roadmap = @user.roadmap
+        sign_in @user
+      end
+
+      context 'with an associated Roadmap' do
+        it 'redirects to their own roadmap' do
+          get '/'
+          expect(response).to render_template('roadmaps/show')
+        end
+      end
+
+      context 'when trying to access someone else\'s roadmap' do
+        let(:other_roadmap) { create(:roadmap, id: 5) }
+
+        it 'redirects to their own roadmap' do
+          get "/roadmaps/#{other_roadmap.id}"
+          expect(response).to render_template('roadmaps/show')
+        end
+      end
+    end
+
+    context 'when user is logged out' do
+      it 'shows the Roadmap homepage' do
+        get '/'
+        expect(response).to render_template('home')
+      end
+    end
+  end
 end
--- spec/system/roadmap_system_spec.rb
+++ spec/system/roadmap_system_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Roadmap", type: :system do
-  it 'lets a Rockstar create a personal Rockmap' do
+  it 'lets a Rockstar sign up' do
     visit '/'
     expect(current_path).to eq root_path
     find('#new-rockmap-btn').click
@@ -13,12 +13,23 @@ RSpec.describe "Roadmap", type: :system do
     fill_in 'user[password_confirmation]', with: 'humanoidx1234'
     find(:xpath, '//input[@type="submit"]').click
 
+    # User is not logged in yet since they are unconfirmed. They are directed to
+    # the root again in this case.
+    expect(current_path).to eq root_path
+  end
+
+  it 'lets Rockstar create a new Rockmap' do
+    login_as(create(:confirmed_blank_user))
+
+    visit '/'
+    expect(current_path).to eq '/roadmaps/new'
+
     fill_in 'roadmap[the_one_thing]', with: 'Advocate Ruby.'
     fill_in 'roadmap[the_why_of_one_thing]', with: 'Because it rocks!'
     fill_in 'roadmap[start_date]', with: '31.12.2020'
     fill_in 'roadmap[the_one_habit]', with: 'Read\'em all!'
-
     find(:xpath, '//input[@type="submit"]').click
+
     expect(page).to have_content 'Your One Thing'
   end
 end
