01b5a42e8e07ba8bde9178efaa27c3a7131c24a3
RichStone <richard.had@hotmail.de>
2021-01-19 22:35:04 +0100

    Fix last_motivator_implementation

--- db/migrate/20210108215025_add_last_motivator_date_to_user.rb
+++ db/migrate/20210108215025_add_last_motivator_date_to_user.rb
@@ -1,5 +1,5 @@
 class AddLastMotivatorDateToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :last_motivator_date, :date, default: Date.today
+    add_column :users, :last_motivator_date, :date, default: -> { 'CURRENT_DATE' }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_215025) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.date "last_motivator_date", default: "2021-01-08"
+    t.date "last_motivator_date", default: -> { "CURRENT_DATE" }
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
