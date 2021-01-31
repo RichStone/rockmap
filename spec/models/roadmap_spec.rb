a0ef72c9fe6f92e454e1c579730a66ff3f6dd5ac
RichStone <richard.had@hotmail.de>
2021-01-31 23:33:15 +0100

    Add roadmap spec examples

--- spec/models/roadmap_spec.rb
+++ spec/models/roadmap_spec.rb
@@ -16,5 +16,34 @@
 require 'rails_helper'
 
 RSpec.describe Roadmap, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:roadmap) { described_class.create!(params) }
+  let(:params) {
+    {
+      the_one_thing: 'pusheen',
+      the_why_of_one_thing: 'cause it\'s neat',
+      the_one_habit: 'grindin',
+      start_date: Date.tomorrow,
+      milestones: [create(:milestone)],
+      accountability_buddies: [create(:accountability_buddy)],
+      user: create(:confirmed_blank_user)
+    }
+  }
+
+  it 'has The One Thing' do
+    # FIXME: don't hard code
+    expect(roadmap.the_one_thing).to eq 'pusheen'
+  end
+  it 'has a user'
+  it 'has a habit to track'
+  it 'starts in future'
+
+  context 'when newly created' do
+    context 'with an accountability buddy defined' do
+      it 'sends out an email to buddy'
+    end
+
+    context 'without a defined accountability buddy' do
+      it 'does not trigger the BuddyMailer'
+    end
+  end
 end
