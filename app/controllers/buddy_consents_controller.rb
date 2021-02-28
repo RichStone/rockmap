c9673e08e9fa8e4dc02c9452db7d7baadadef338
RichStone <richard.had@hotmail.de>
2021-02-28 06:48:59 +0100

    Redirect to root with failure or success toasts

--- app/controllers/buddy_consents_controller.rb
+++ app/controllers/buddy_consents_controller.rb
@@ -4,5 +4,10 @@ class BuddyConsentsController < ApplicationController
     consent.accountability_buddy.update!(accountability_consent_given: true)
     # Consent instance is not needed anymore
     consent.delete
+    flash[:success] = 'Awesome, you are now an accountability buddy!'
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'Something went wrong, maybe you\'ve already clicked that link?'
+  ensure
+    redirect_to root_url
   end
 end
--- spec/requests/buddy_consents_spec.rb
+++ spec/requests/buddy_consents_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe 'BuddyConsents', type: :request do
   describe 'GET /consent_given' do
     let(:buddy) { create(:accountability_buddy, buddy_consent: consent)}
     let(:consent) { create(:buddy_consent) }
+    let(:consent_id) { buddy.buddy_consent.id }
 
-    let(:consent_request) { get "/consent_given/#{buddy.buddy_consent.id}"; buddy.reload }
+    let(:consent_request) { get "/consent_given/#{consent_id}"; buddy.reload }
 
     it 'confirms the buddy' do
       expect { consent_request }
@@ -22,6 +23,23 @@ RSpec.describe 'BuddyConsents', type: :request do
     it 'does not create a new consent when attributes are updated' do
       consent_request
       expect(BuddyConsent.count).to eq 0
+    end
+
+    context 'when successful' do
+      it 'redirects to root with success toast' do
+        consent_request
+        expect(response).to redirect_to(root_url)
+        expect(flash[:success]).to be_present
+      end
+    end
+
+    context 'when already used activation code' do
+      let(:consent_id) { 'fake-id' }
+      it 'redirects to root with failure toast' do
+        consent_request
+        expect(response).to redirect_to(root_url)
+        expect(flash[:alert]).to be_present
+      end
     end
   end
 end
